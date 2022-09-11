9f95969b8f9bdcf43dfa00943f2c201f81cf2984
Rafael Costa <54642754+gitreis@users.noreply.github.com>
2022-09-11 20:42:37 -0300

    Create READMe.d

---
+++ Desafio: Social Tree/README.d
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>💻 Sobre o desafio</h1>
+    <p>
+      Neste desafio você deve criar uma página com uma lista de links que pode ser usada em perfis de redes sociais.
+    </p>
+  <h1>Layout</h1>
+    <p>
+      ![MacBook Pro 16 inch.png](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/152cec1f-b8ad-4b3a-9520-e1fbd5433a32/MacBook_Pro_16_inch.png)
+    </p>
+      Você pode duplicar o layout do projeto [clicando aqui](https://www.figma.com/file/yi1ycIyAW8QiGiX9bMFHkU/DD-%2F-Social-links/duplicate) ou baixar o zip com os assets do projeto.
+    <p>
+      Baixe os assets aqui ⤵️
+    </p>
+      <a href="https://s3-us-west-2.amazonaws.com/secure.notion-static.com/51256d0c-9ddf-4191-81a8-350996607090/assets.r">arassets.rar</a>
+  <h1>🚀 Techs</h1>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+    </ul>
+
+  <h1>💡Como começar?</h1>
+
+    <ul>
+      <li>1 - Use o link do Figma como base para o projeto. Também disponibilizamos para download todos os assets necessários (imagens e ícones), para fazer o download basta clicar no link acima.</li>
+      <li>2 - Leia com atenção todas as instruções do desafio.</li>
+      <li>3 - Bora codar! Lembre-se que você pode usar as tecnologias que se sentir mais confortável, mas também pode se desafiar usando novas techs, fazendo modificações e/ou adicionando funcionalidades no projeto como preferir. 🚀</li>
+      <li>4 - Compartilhe seu resultado ou tire suas dúvidas na nossa <a href="https://discord.gg/bacwY2gDCF">comunidade aberta</a>
+      </li>
+    </ul>
+
+  <h1>✅ Requisitos</h1>
+    <p>Neste desafio você vai construir *uma página com uma lista de links.* Caso você ainda não tenha feito os cursos do Discover ou queira fazer uma revisão, segue abaixo uma lista dos cursos que podem te ajudar a resolver este desafio.</p>
+    <br>
+    <ul>
+      <li><a href="https://app.rocketseat.com.br/node/o-guia-estelar-de-html">O guia estelar de HTML</a></li>
+      <li><a href="https://app.rocketseat.com.br/node/o-guia-estelar-de-css">O guia estelar de CSS</a></li>
+    </ul>
+    <br>
+    <p>Requisitos para o desafio:</p>
+    <p>
+      <ul>
+        <li>Adicione uma imagem de perfil</li>
+        <li>Adicione uma lista de links que pode ser usada para enviar o usuário para outros sites</li>
+        <li>Opcional: Adicione ícones com links para outras redes sociais</li>
+        <li>**Dica:** para centralizar o conteúdo você pode adicionar um elemento (div) com uma largura fixa e depois adicionar `margin: 0 auto`</li>
+        <li>Você poderá usar como modelo o layout do Figma disponibilizado acima.</li>
+      </ul>
+    </p>
+    <h1>🎨 Style Guide</h1>
+    <br>
+    <h2>Cores:</h2>
+    <br>
+    <br>
+    <p>
+      :root {
+        --body-bg-color: #FAF7FD;
+        --purple: #BB99DD;
+      }
+    </p>
+    <h2>Tipo de fonte:</h2>
+      <p>
+        font-family: Poppins
+
+        Font Weight: 300 e 600
+
+        <a href="https://fonts.google.com/">Você pode encontrar a fonte no Google Fonts</a>
+      </p>
+</body>
+</html>
